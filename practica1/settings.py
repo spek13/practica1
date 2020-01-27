c1d667d684ee1889b4851aeba3c56950b46dd9c3
spek13 <171221@ids.upchiapas.edu.mx>
2020-01-27 15:50:41 -0600

    base de datos

--- practica1/settings.py
+++ practica1/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'practica1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'NAME': os.path.join(BASE_DIR, 'db.database-2'),
+        'USER': 'postgres',
+        'PASSWORD': 'master12345',
+        'HOST': 'database-2.cz7iy75fggfe.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
+        'NAME': 'postgres'
     }
 }
 
