49d94a88508e4ef08b1150268e85735e5736747f
spek13 <171221@ids.upchiapas.edu.mx>
2020-02-11 15:53:58 -0600

    ok3

--- practica1/urls.py
+++ practica1/urls.py
@@ -35,7 +35,7 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^',include(router.urls)),
-    re_path(r'^api/v1/Login', include('Login.urls')),
+    #re_path(r'^api/v1/Login', include('Login.urls')),
     re_path(r'^api/v1/', include('Login.urls')),
 ]
 
